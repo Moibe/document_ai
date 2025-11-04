20c2c80f5204de544b6b1c41e25ad455d96cc423
Moibe <moi.estrello@gmail.com>
2025-11-04 16:25:28 -0600

    httpx

--- funciones.py
+++ funciones.py
@@ -84,14 +84,12 @@ async def procesa_pasaporte(image: UploadFile):
 
 async def procesa_fm(image: UploadFile):
     
-    # Obtener la cadena Base64
+ 
     base64_content = await herramientas.upload_a_base64(image)
         
-    # Obtener el MIME type (FastAPI lo da directamente)
     mime_type = image.content_type
     print("Mimetype es: ", mime_type)
     
-    # Estructura del JSON para Document AI
     processor_json = {
         "rawDocument": {
             "mimeType": mime_type,
@@ -99,7 +97,6 @@ async def procesa_fm(image: UploadFile):
         }
     }
 
-    # Definir las cabeceras con el token de autenticación
     headers = {
         "Authorization": f"Bearer {access_token}",
         "Content-Type": "application/json; charset=utf-8"
@@ -137,6 +134,6 @@ async def procesa_fm(image: UploadFile):
     
     #entidades = herramientas.obtener_estructura_limpia_para_fastapi(data_json)
     #entidades = herramientas.imprimir_entidades(data_json)
-    entidades = herramientas.simplificar_entidades_pasaporte(data_json)
-    #entidades = herramientas.obtener_datos_pasaporte_completos(data_json)
+    #entidades = herramientas.simplificar_entidades_pasaporte(data_json)
+    entidades = herramientas.obtener_datos_pasaporte_completos(data_json)
     return entidades 
